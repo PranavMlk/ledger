b6ecd048d832e4c731840c9ab3d048ac10be4c47
Pranav_MLK <110965548+PranavMlk@users.noreply.github.com>
2023-03-14 12:02:44 +0530

    Add files via upload

---
+++ lib/pages/income_page.dart
@@ -0,0 +1,242 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../controller/db_helper.dart';
+import '../models/transaction_model.dart';
+import '../widget/dailogue_confirm.dart';
+
+class Incomes extends StatefulWidget {
+  const Incomes({Key? key}) : super(key: key);
+
+  @override
+  State<Incomes> createState() => _IncomesState();
+}
+
+class _IncomesState extends State<Incomes> {
+
+  late Box box;
+  late SharedPreferences preferences;
+  DbHelper dbHelper = DbHelper();
+
+  List<String> months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
+
+  Future<List<TransactionModel>> fetch() async {
+    if (box.values.isEmpty) {
+      return Future.value([]);
+    } else {
+      // return Future.value(box.toMap());
+      List<TransactionModel> items = [];
+      box.toMap().values.forEach((element) {
+        // print(element);
+        items.add(
+          TransactionModel(
+            element['amount'] as int,
+            element['note'],
+            element['date'] as DateTime,
+            element['type'],
+          ),
+        );
+      });
+      return items;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getPreference();
+    box = Hive.box('money');
+    fetch();
+}
+
+  getPreference() async {
+    preferences = await SharedPreferences.getInstance();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade700,
+      appBar: AppBar(
+        title: Text('Cashbook'),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: <Color>[
+                  Colors.black,
+                  Colors.red]),
+          ),
+        ),
+      ),
+      persistentFooterButtons: [
+        Container(
+          width: 900,
+          child: Text(
+            '©Pranav',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: Colors.black,
+                letterSpacing: 1,
+                fontWeight: FontWeight.bold
+            ),
+          ),
+        )
+      ],
+      body: FutureBuilder<List<TransactionModel>>(
+          future:fetch(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError){
+              return Center(child: Text("Error!"),);
+            }
+            if (snapshot.hasData){
+              if(snapshot.data!.isEmpty)
+              {}
+              return ListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child:Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 20,
+                            horizontal: 6
+                          ),
+                          child: Text('Your Incomes',
+                            style: TextStyle(
+                              fontSize: 26.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                        ),
+                        ListView.builder(
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          itemCount: snapshot.data!.length + 1,
+                          itemBuilder: (context, index) {
+                            TransactionModel dataAtIndex;
+                            try {
+                              dataAtIndex = snapshot.data![index];
+                            } catch (e) {
+                              return Container();
+                            }
+                            switch(dataAtIndex.type) {
+                              case "Income": {
+                                return IncomeTile(dataAtIndex.amount, dataAtIndex.note, dataAtIndex.date, index);
+                              }
+                            }
+                          }
+                  ),
+                      ],
+                    ),
+                  )
+                  ]
+              );
+            }
+            else{
+              return Center(child: Text("Error!"),);
+
+            }
+
+          }
+      ),);
+  }
+
+  Widget IncomeTile(int value, String note,DateTime date, int index){
+    return InkWell(
+      onLongPress: () async {
+        bool? answer = await showConfirmDialog(
+          context,
+          "WARNING",
+          "This will delete this expense record. This action is irreversible. Do you want to continue ?",
+        );
+        if (answer != null && answer) {
+          await dbHelper.deleteData(index);
+          setState(() {});
+        }
+      },
+      child: Container(
+        margin: EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(18.0),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Colors.greenAccent,
+              Colors.green
+            ]),
+            borderRadius: BorderRadius.circular(8.0) ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Icon(Icons.arrow_circle_up_outlined,
+                      size: 28.0,
+                      color: Colors.green[800],
+                    ),
+                    SizedBox(
+                      width: 4.0,
+                    ),
+                    Text("Income",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    )
+
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: Text("${date.day} ${months[date.month -1]} ${date.year}",
+                    style: TextStyle(
+                      color: Colors.grey[800],
+                      // fontSize: 24.0,
+                      // fontWeight: FontWeight.w700
+
+                    ),
+                  ),
+                ),
+
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(" + $value Rs",
+                  style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w700),
+                ),
+                Text(note,
+                  style: TextStyle(
+                    color: Colors.grey[800],
+                    // fontSize: 24.0,
+                    // fontWeight: FontWeight.w700
+
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/login.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+import 'package:ledger/pages/homepage.dart';
+import 'package:ledger/pages/sign_up.dart';
+import '../controller/sql_helper.dart';
+import 'admin.dart';
+import 'login_signup.dart';
+
+class Login_Form extends StatefulWidget {
+  @override
+  State<Login_Form> createState() => _Login_FormState();
+}
+
+class _Login_FormState extends State<Login_Form> {
+  var formkey = GlobalKey<FormState>();
+  final TextEditingController conemail = TextEditingController();
+  final TextEditingController conpass = TextEditingController();
+
+  void logincheck (String email,String password) async{
+    if( email == 'admin@gmail.com' &&  password == '12345678'){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>AdminHome()));
+
+    }else {
+      var data = await SQLHelper.CheckLogin(email, password);
+      if(data.isNotEmpty){
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> Homepage()));
+        print('Login Success');
+      }else if(data.isEmpty){
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> Login_Signup()));
+        print('Login faild');
+      }
+    }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool hidepass = true;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cashbook'),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: <Color>[
+                  Colors.black,
+                  Colors.red,
+                ]),
+          ),
+        ),
+      ),
+      persistentFooterButtons: [
+        Container(
+          width: 900,
+          child: Text(
+            '©Pranav',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: Colors.black,
+                letterSpacing: 1,
+                fontWeight: FontWeight.bold
+            ),
+          ),
+        )
+      ],
+      body: Form(
+        key: formkey,
+        child: ListView(
+          children: [
+            Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: Text(
+                    "Login Page",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                )),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextFormField(
+                controller: conemail,
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.drive_file_rename_outline),
+                    labelText: "Email",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    )),
+                validator: (value) {
+                  if (value != null) {
+                    if (value.contains('@') && value.endsWith('.com')) {
+                      return null;
+                    }
+                    return 'Enter a Valid Email Address';
+                  }
+                },
+                textInputAction: TextInputAction.next,
+              ),
+            ),
+            Padding(
+                padding: EdgeInsets.all(20),
+                child: TextFormField(
+                  controller: conpass,
+                  obscureText: hidepass,
+                  obscuringCharacter: "*",
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          if (hidepass)
+                            hidepass = false;
+                          else
+                            hidepass = true;
+                        });
+                      },
+                      icon: Icon(
+                          hidepass ? Icons.visibility : Icons.visibility_off),
+                    ),
+                    labelText: "Password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  validator: (pass) {
+                    if (pass == null || pass.trim().isEmpty) {
+                      return 'This field is required';
+                    }
+                    if (pass.trim().length < 8) {
+                      return 'Password must be at least 8 characters in length';
+                    }
+                    return null;
+                  },
+                  textInputAction: TextInputAction.next,
+                )),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                    Colors.black,
+                    Colors.red
+                  ]
+                  ),
+                  borderRadius: BorderRadius.circular(25)
+                ),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.transparent,
+                    onSurface: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                  ),
+                  onPressed: () {
+                    final valid = formkey.currentState!.validate();
+
+                    if(valid) {
+                      logincheck(conemail.text,conpass.text);
+                    } else {
+
+                    }
+                  },
+                  child: Text("LOGIN"),
+                ),
+              ),
+            ),
+            TextButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>  Signup_Form()));
+                }, child: Text('Not a User? Register Here!!!',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 15,
+                fontWeight: FontWeight.w700
+              ),
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
